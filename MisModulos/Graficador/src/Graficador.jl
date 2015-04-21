560113258e4f2b74a79bb0b63f56895f54cf9ec3
Santiago Hernandez Gomez <shgaeo@ciencias.unam.mx>
2015-04-20 23:52:51 -0500

    agregué función grafica areas

--- MisModulos/Graficador/src/Graficador.jl
+++ MisModulos/Graficador/src/Graficador.jl
@@ -2,7 +2,7 @@
 
 module Graficador
 
-export graficador
+export graficador, graficaarea
 
 using PyPlot, Intervalos    
 
@@ -37,6 +37,30 @@ function graficador(a,b,f::Function)
 
     PyPlot.fill_between([a,b],float64(polY.a),float64(polY.b),color="lightgray")
     PyPlot.suptitle("La sombra gris es la zona calculada con intervalo [$a,$b]")
+end
+
+function graficaarea(a,b,f::Function)
+    puntosX=(a-2:0.125:b+2)
+    puntosY=similar(puntosX)
+    for i=1:length(puntosX)
+        puntosY[i]=f(puntosX[i])
+    end
+    
+    ptsX=(a:0.125:b)
+    ptsY=similar(ptsX)
+    for i=1:length(ptsX)
+        ptsY[i]=f(ptsX[i])
+    end
+    
+    polY=f(Intervalo(a,b))
+    
+    PyPlot.plot(puntosX,puntosY,"r",linewidth=1)
+    PyPlot.plot(ptsX, ptsY, "b", linewidth=2.0)
+        
+    PyPlot.xlim(min(-1,a-1),max(0,b+1))
+    PyPlot.ylim(min(-1,float64(polY.a)-1.5),max(float64(polY.a),float64(polY.b),f(a),f(b))+1);
+
+    PyPlot.fill_between([a,b],float64(polY.a),float64(polY.b),color="lightgray")
 end
 
 end
